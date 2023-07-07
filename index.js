89c6e16f590dbdb073db6f6f166c826b4bf7dc65
Austin Pearson <124529429+Apearson14@users.noreply.github.com>
2023-07-07 17:14:59 -0400

    Task 2.4

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+
+app.use(morgan('combined'));
+
+// Creating GET route at endpoint "/movies" returning JSON object (Returns all movies)
+app.get('/movies',(req,res)=>{
+    res.json(movies);
+})
+
+app.get('/', (req, res) => {
+    res.send('Welcome to my movie collection!');
+  });
+
+app.use(express.static('public'));
+
+// Error-handling middleware
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500).send('Something went wrong');
+  });
