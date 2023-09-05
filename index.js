1cff6419945b34cc6772bf19cd0417ed07c10657
Austin Pearson <124529429+Apearson14@users.noreply.github.com>
2023-09-05 19:48:09 -0400

    Adding user methods

--- index.js
+++ index.js
@@ -124,8 +124,30 @@ app.get('/movies/directors/:directorName',(req,res) => {
   });
 
 // Allow new users to register
-app.post('/users', )
+app.post("/users",(req,res) =>{
+  const newUser = req.body;
 
+  if(newUser.name) {
+    newUser.id = uuid.v4();
+    users.push(newUser);
+    res.status(201).json(newUser);
+  } else {
+    res.status(400).send("name?");
+  }
+});
+
+// Allow users to update their user info (username)
+app.put('/users/:id',(req,res) => {
+  const {id} = req.params;
+  const updatedUser = req.body;
+
+  let user = users.find((user)=> user.id == id);
+  
+  if(user){
+    user.name = updatedUser.name;
+    res.status(200).json(user);
+  }
+});
 
 app.use(express.static('public'));
 
